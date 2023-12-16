95763d1a038b99fc207ff7cdf11842165d3b5477
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2023-12-16 21:24:17 +0530

    User model added

---
+++ lib/models/user.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class User {
+  final String email;
+  final String uid;
+  final String photoUrl;
+  final String username;
+  final String bio;
+  final List followers;
+  final List following;
+
+  User({
+    required this.email,
+    required this.uid,
+    required this.photoUrl,
+    required this.username,
+    required this.bio,
+    required this.followers,
+    required this.following,
+  });
+
+  Map<String, dynamic> toJson()  => {
+    'username': username,
+    'uid': uid,
+    'email': email,
+    'photoUrl': photoUrl,
+    'bio': bio,
+    'followers': followers,
+    'following': following,
+  };
+}
--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/models/user.dart' as model;
 import 'package:instagram_clone/resources/storage_methods.dart';
 
 class AuthMethods {
@@ -36,15 +37,18 @@ class AuthMethods {
             .uploadImageToStorage("profilePics", file, false);
 
         // add user to the database
-        await _firestore.collection("users").doc(cred.user!.uid).set({
-          "userName": username,
-          "uid": cred.user!.uid,
-          "email": email,
-          "bio": bio,
-          "followers": [],
-          "following": [],
-          "photoUrl": photoUrl,
-        });
+
+        model.User user = model.User(
+          email: email,
+          uid: cred.user!.uid,
+          photoUrl: photoUrl,
+          username: username,
+          bio: bio,
+          followers: [],
+          following: [],
+        );
+
+        await _firestore.collection("users").doc(cred.user!.uid).set(user.toJson());
 
         res = "Success";
       } else {
