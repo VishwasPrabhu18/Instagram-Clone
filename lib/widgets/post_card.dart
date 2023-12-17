f6ae013a2df6b367c43c4fccedb9d1d19048603b
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2023-12-17 21:31:38 +0530

    Feed Screen Layouting1

---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+class PostCard extends StatelessWidget {
+  const PostCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: mobileBackgroundColor,
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 4,
+              horizontal: 16,
+            ).copyWith(right: 0),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 16,
+                  backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1682686578456-69ae00b0ecbd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw0Mnx8fGVufDB8fHx8fA%3D%3D",
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "UserName",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
