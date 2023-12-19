94f5e2abf1752affbede3177ddc1b55316d6c1ba
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2023-12-19 19:46:40 +0530

    bottom navigation added

--- lib/screens/comments_screen.dart
+++ lib/screens/comments_screen.dart
@@ -17,6 +17,46 @@ class _CommentsScreenState extends State<CommentsScreen> {
         title: const Text("Comments"),
         centerTitle: false,
       ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: kToolbarHeight,
+          margin:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          padding: const EdgeInsets.only(left: 16, right: 8),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1678875202809-4c7c9d117128?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfE04alZiTGJUUndzfHxlbnwwfHx8fHw%3D",
+                ),
+                radius: 18,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 8),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Comment as username",
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  child: const Text(
+                    "Post",
+                    style: TextStyle(color: blueColor),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
