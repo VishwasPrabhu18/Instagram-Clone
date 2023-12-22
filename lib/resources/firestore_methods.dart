c15ba466ffe99583cbaede9d7c7fa9b82880666d
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2023-12-22 17:02:25 +0530

    delete firestore method is done

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -89,4 +89,13 @@ class FireStoreMethods {
       print(err.toString());
     }
   }
+
+  // deleteing the posts
+  Future<void> deletePost(String postId) async {
+    try {
+      await _firestore.collection("posts").doc(postId).delete();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 }
