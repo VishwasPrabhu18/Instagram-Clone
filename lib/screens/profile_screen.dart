447d711c0dd253d06df96dbe0057ddfa9057bd25
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2023-12-22 19:30:41 +0530

    profile image ui added

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: Text("UserName"),
+        centerTitle: false,
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      backgroundImage: NetworkImage(
+                        "https://images.unsplash.com/photo-1703165805602-54276ce0cd6b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHx8",
+                      ),
+                      radius: 40,
+                    ),
+
+                    
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
