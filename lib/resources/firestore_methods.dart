f44977e0a74908a11c29b44ddf09f0373f8ca7df
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2023-12-26 12:48:24 +0530

    firestore method is done for following

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -98,4 +98,31 @@ class FireStoreMethods {
       print(e.toString());
     }
   }
+
+  Future<void> followUser(
+    String uid,
+    String followId,
+  ) async {
+    try {
+      DocumentSnapshot snap =
+          await _firestore.collection("users").doc(uid).get();
+      List following = (snap.data()! as dynamic)["following"];
+
+      if (following.contains(followId)) {
+        await _firestore.collection("users").doc(followId).update({
+          "following": FieldValue.arrayRemove([uid])
+        });
+        await _firestore.collection("users").doc(uid).update({
+          "followers": FieldValue.arrayRemove([uid])
+        });
+      } else {
+        await _firestore.collection("users").doc(followId).update({
+          "followers": FieldValue.arrayUnion([uid])
+        });
+
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 }
