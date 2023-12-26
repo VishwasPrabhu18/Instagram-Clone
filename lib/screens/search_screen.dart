bc27d77e9ec4d05a85adc6fd84337d0da6246762
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2023-12-26 15:11:29 +0530

    search screen updated for web

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram_clone/screens/profile_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/global_variables.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -94,7 +95,10 @@ class _SearchScreenState extends State<SearchScreen> {
                   itemBuilder: (context, index) => Image.network(
                     (snapshot.data! as dynamic).docs[index]["postUrl"],
                   ),
-                  staggeredTileBuilder: (index) => StaggeredTile.count(
+                  staggeredTileBuilder: (index) => MediaQuery.of(context).size.width > webScreenSize? StaggeredTile.count(
+                    (index % 7 == 0) ? 1 : 1,
+                    (index % 7 == 0) ? 1 : 1,
+                  ) : StaggeredTile.count(
                     (index % 7 == 0) ? 2 : 1,
                     (index % 7 == 0) ? 2 : 1,
                   ),
